7e6ef824bae3807cde18f1a062ce9811f6136da0
arsalanqasim <arsalanqasim400@gmail.com>
2026-07-25 16:27:43 +0500

    fix: change playwright webServer command to be cross-platform on master

--- playwright.config.ts
+++ playwright.config.ts
@@ -31,7 +31,7 @@ export default defineConfig({
     video: 'retain-on-failure',
   },
   webServer: {
-    command: 'npm.cmd run preview -- --host 127.0.0.1 --port 4173',
+    command: 'npm run preview -- --host 127.0.0.1 --port 4173',
     url: 'http://127.0.0.1:4173',
     reuseExistingServer: !process.env.CI,
     timeout: 120_000,
